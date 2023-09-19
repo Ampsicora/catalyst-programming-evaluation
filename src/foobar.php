1c648b4737741240a1cf71cb77019791e67d8e33
Ampsicora <ampsicora@pm.me>
2023-09-19 16:40:08 +0200

    add run function with exercise logic

--- src/foobar.php
+++ src/foobar.php
@@ -4,6 +4,21 @@ declare(strict_types=1);
 
 class LogicTest
 {
+    public function run(string $separator = ', '): void
+    {
+        for ($i = 1; $i <= 100; $i++) {
+            $word = '';
+
+            if ($this->is_divisible_by(3, $i))
+                $word .= 'foo';
+
+            if ($this->is_divisible_by(5, $i))
+                $word .= 'bar';
+
+            echo (empty($word) ? $i : $word) . $separator;
+        }
+    }
+
     public function is_divisible_by(int $divisor, int $number): bool
     {
         return $number % $divisor === 0;
